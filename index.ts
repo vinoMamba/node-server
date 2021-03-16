6508859eed01e77f76c24459940302db516d6a05
vino <vino0908@outlook.com>
2021-03-16 21:27:51 +0800

    创建项目

--- index.ts
+++ index.ts
@@ -2,8 +2,9 @@ import * as http from 'http';
 
 const server = http.createServer();
 
-server.on('request', () => {
+server.on('request', (request, response) => {
     console.log('有人请求了');
+    response.end('hi');
 });
 
 server.listen(8888);
