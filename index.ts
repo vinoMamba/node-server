3dfe548167fa31ae5649bdbea6325298b4fc3526
vino <vino0908@outlook.com>
2021-03-17 11:51:18 +0800

    支持curl查询参数

--- index.ts
+++ index.ts
@@ -1,37 +1,38 @@
 import * as http from 'http';
-import {IncomingMessage, ServerResponse} from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import * as fs from 'fs';
-import * as path from 'path';
+import * as p from 'path';
 
-const publicDir = path.resolve(__dirname, 'public');
+const publicDir = p.resolve(__dirname, 'public');
 const server = http.createServer();
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const {method, url, headers} = request;
-    switch (url) {
+    const { method, url: baseUrl, headers } = request;
+    const url = new URL('http://localhost' + baseUrl)
+    const pathName = url.pathname
+    switch (pathName) {
         case '/index.html':
             response.setHeader('content-type', 'text/html; charset=utf-8');
-            fs.readFile(path.resolve(publicDir, 'index.html'), (error, data) => {
+            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
                 if (error) throw error;
-                response.end(data.toString());
+                response.end(data);
             });
             break;
         case '/index.css':
             response.setHeader('content-type', 'text/css; charset=utf-8');
-            fs.readFile(path.resolve(publicDir, 'index.css'), (error, data) => {
+            fs.readFile(p.resolve(publicDir, 'index.css'), (error, data) => {
                 if (error) throw error;
-                response.end(data.toString());
+                response.end(data);
             });
             break;
         case '/main.js':
             response.setHeader('content-type', 'text/javascript; charset=utf-8');
-            fs.readFile(path.resolve(publicDir, 'main.js'), (error, data) => {
+            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
                 if (error) throw error;
-                response.end(data.toString());
+                response.end(data);
             });
             break;
     }
-
 });
 
 server.listen(8888, () => {
