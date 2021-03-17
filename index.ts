ddc653abb52814cb467df38990e04feee3197f13
vino <vino0908@outlook.com>
2021-03-17 22:08:32 +0800

    处理无路径的情况，重定向到index.html

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as http from 'http';
-import { IncomingMessage, ServerResponse } from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
 
@@ -7,24 +7,26 @@ const publicDir = p.resolve(__dirname, 'public');
 const server = http.createServer();
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const { method, url: baseUrl, headers } = request;
-    const url = new URL('http://localhost' + baseUrl)
-    console.log(url)
-    const pathName = url.pathname
-    const fileDir = url.pathname.substr(1)
+    const {method, url: baseUrl, headers} = request;
+    const url = new URL('http://localhost' + baseUrl);
+    console.log(url);
     // response.setHeader('content-type', 'text/html; charset=utf-8');
+    let fileDir = url.pathname.substr(1);
+    if (fileDir === '') {
+        fileDir = 'index.html';
+    }
     fs.readFile(p.resolve(publicDir, fileDir), (error, data) => {
         if (error) {
-            console.log(error)
+            console.log(error);
             if (error.errno === -4058) {
-                response.statusCode = 404
+                response.statusCode = 404;
                 fs.readFile(p.resolve(publicDir, 'notFoundPage.html'), (error, data) => {
-                    if (error) throw error
-                    response.end(data)
-                })
+                    if (error) throw error;
+                    response.end(data);
+                });
             } else {
-                response.statusCode = 500
-                response.end('服务异常')
+                response.statusCode = 500;
+                response.end('服务异常');
             }
         } else {
             response.end(data);
@@ -32,6 +34,4 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     });
 });
 
-server.listen(8888, () => {
-    console.log(server.address());
-});
+server.listen(8888);
