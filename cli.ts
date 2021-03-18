4dd64c6316ee4a6e0e61a72fcbb5a47d859c3008
vino <vino0908@outlook.com>
2021-03-18 17:13:38 +0800

    minimist代替commander

--- cli.ts
+++ cli.ts
@@ -1,9 +1,22 @@
-import {program} from 'commander';
-
-program
-    .option('-f, --fileName <fileName>')
-    .option('-p, --port <port>', 'Listen One Port')
-    .option('-c, --cache <number>', 'Setting Cache-Control')
-    .option('-p, --port <port>', 'listen one port');
-
-program.parse(process.argv);
+const argv = require('minimist')(process.argv.slice(2));
+const param = {
+    filePath: '',
+    cacahe: 365,
+    address: '127.0.0.1'
+}
+if (argv._.length === 0) {
+    showHelpMessage()
+} else {
+    param.filePath = argv._[0]
+}
+//帮助信息
+function showHelpMessage() {
+    console.log(
+        `options:
+  -p --port    Setting Port [8080]
+  -a --address Setting Address [0.0.0.0]
+  -c --cache   Setting Cache-Control [365]
+  -h --help    Get Help`
+    )
+}
+export default param
