a124b694cf5d78924755c671b974205aca4e3f2e
vino <vino0908@outlook.com>
2021-03-18 18:41:48 +0800

    设置content-type

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import * as http from 'http';
-import {IncomingMessage, ServerResponse} from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
 
@@ -7,7 +7,7 @@ const publicDir = p.resolve(__dirname, 'public');
 const server = http.createServer();
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const {method, url: baseUrl, headers} = request;
+    const { method, url: baseUrl, headers } = request;
     if (method !== 'GET') {
         response.statusCode = 405;
         response.end();
@@ -15,7 +15,14 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     }
     const url = new URL('http://localhost' + baseUrl);
     console.log(url);
-    // response.setHeader('content-type', 'text/html; charset=utf-8');
+    const index = url.pathname.indexOf('.')
+    const fileSuffix = url.pathname.substr(index)
+    const fileType = {
+        '.html': 'html',
+        '.css': 'css',
+        '.js': 'javascript'
+    }
+    response.setHeader('content-type', `text/${fileType[fileSuffix]}; charset=utf-8`);
     let fileDir = url.pathname.substr(1);
     if (fileDir === '') {
         fileDir = 'index.html';
@@ -35,7 +42,7 @@ server.on('request', (request: IncomingMessage, response: ServerResponse) => {
             }
         } else {
             //返回文件内容
-            response.setHeader('Cache-Control', 'public, max-age= 31536000');
+            response.setHeader('Cache-Control', `public, max-age= 31536000`);
             response.end(data);
         }
     });
